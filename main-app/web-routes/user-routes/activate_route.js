d278d525841e56f59065e549e146d03f9641822e
Nan Qin <qinnan0416@gmail.com>
2019-04-25 16:09:32 -0400

    add cros to activate

--- main-app/web-routes/user-routes/activate_route.js
+++ main-app/web-routes/user-routes/activate_route.js
@@ -5,7 +5,7 @@
 const express = require('express');
 const has_value = require('../../helper_functions').has_value;
 const activateRouter = express.Router();
-
+const cors = require('../cors');
 const activateEmail = require('../../db_operations/email_verification_db').activateEmail;
 
 
@@ -13,7 +13,10 @@ const activateEmail = require('../../db_operations/email_verification_db').activ
 
 
 activateRouter.route("/:value")
-.get((req, res, next)=>{
+.options(cors.cors, (req, res, next) => {
+    res.sendStatus(200);
+})
+.get(cors.cors,(req, res, next)=>{
     activateEmail(req.params.value, (err, user)=>{
         if(err){
             res.statusCode = 403;
